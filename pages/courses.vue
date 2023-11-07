4bc0b33a3acd10d78d77daf31f7082b57297445b
tlkhanh <longkhanhta@gmail.com>
2023-11-07 22:42:50 +0700

    dynamic routing

--- pages/courses.vue
+++ pages/courses.vue
@@ -1,4 +1,19 @@
 
 <template>
-Courses work
+<div class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center">
+    <div class="prose mb-12">
+        <h1 class="text-5xl mb-2">
+            <span class="font-medium">Course: </span>
+            <span class="font-bold">Nuxt 3</span>
+        </h1>
+    </div>
+    <div class="flex flex-row justify-center flex-grow">
+        <div class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col">
+            <h3 class="text-3xl mb-2 font-medium">Chapters</h3>
+        </div>
+        <div class="prose p-12 bg-white rounded-md w-[65ch]">
+            <NuxtPage />
+        </div>
+    </div>
+</div>
 </template>
---
+++ pages/courses/chapter-[chapterSlug]/lesson-[lessonSlug].vue
@@ -0,0 +1,5 @@
+<template>
+    <h2 class="text-4xl font-medium mb-2">Lesson</h2>
+    <p>This is a lesson</p>
+    {{ $route.params }}
+</template>
